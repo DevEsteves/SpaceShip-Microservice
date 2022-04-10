41bd8ce61a550ae6b7e22f0a75f896e893ac8a04
devesteves <estevesrodri@outlook.com>
2022-04-09 23:27:35 -0300

    Add SpaceShipRestController

--- Micronaut-Microservice/space/src/main/java/example/micronaut/space/SpaceShipRestController.java
+++ Micronaut-Microservice/space/src/main/java/example/micronaut/space/SpaceShipRestController.java
@@ -1,7 +1,32 @@
-package example.micronaut.space;
+package com.example.micronaut.space;
 
+import io.micronaut.http.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
+@Controller
+@RequestMapping("/space")
 public class SpaceShipRestController {
-    
+    private static List<SpaceShip> ships = new ArrayList(List.of(new SpaceShip( "Mike", 60 )));
+
+    @Get("/ship")
+    public List<SpaceShip> ships(){
+        return ships;
+    }
+
+    @Post("/ship")
+    public List<SpaceShip> ships(@Body SpaceShip ship){
+        ships.add(ship);
+        return ships;
+    }
+
+    @Delete("/ship/{captain}")
+    public List<SpaceShip> deleteships(@PathVariable String captain){
+        List<SpaceShip> collect = ships.stream().filter((s) -> s.getCaptain().equalsIgnoreCase(captain))
+            .collect(Collectors.toList());
+        ships.removeAll(collect);
+        return ships;
+    } 
 }
