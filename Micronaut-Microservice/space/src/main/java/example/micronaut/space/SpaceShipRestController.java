6dad070a726a373f9e473d68e9992a91646fb68d
devesteves <estevesrodri@outlook.com>
2022-04-11 19:36:44 -0300

    Update SpaceShipRestController class

--- Micronaut-Microservice/space/src/main/java/example/micronaut/space/SpaceShipRestController.java
+++ Micronaut-Microservice/space/src/main/java/example/micronaut/space/SpaceShipRestController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/space")
 public class SpaceShipRestController {
     private static List<SpaceShip> ships = new ArrayList(List.of(new SpaceShip( "Mike", 60 )));
 
