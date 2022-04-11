454753e1cf73fe3c0b85a3e75f1e8e886d69345d
devesteves <estevesrodri@outlook.com>
2022-04-11 19:34:09 -0300

    Update SpaceShip class

--- Micronaut-Microservice/space/src/main/java/example/micronaut/space/SpaceShip.java
+++ Micronaut-Microservice/space/src/main/java/example/micronaut/space/SpaceShip.java
@@ -3,9 +3,11 @@ package example.micronaut.space;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SpaceShip {
     private String captain;
     private int fuel;
